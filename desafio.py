714fbe302c0a2a7ac73a0c8a6122f1af89bf8386
iescheinberg <agr.scheinberg@gmail.com>
2023-05-16 10:56:01 -0300

    primer commit

---
+++ desafio.py
@@ -0,0 +1,23 @@
+# Desafio_1
+
+# empleado1 = input('Ingrese su nombre: ')
+# ventas_emp1 = int(input('Ingrese el valor total de sus ventas mensuales $: '))
+# empleado2 = input('Ingrese su nombre: ')
+# ventas_emp2 = int(input('Ingrese el valor total de sus ventas mensuales $: '))
+# comision_emp1 = ventas_emp1*0.06
+# comision_emp2 = ventas_emp2*0.06
+# print(f'\n{empleado1} vendió el ultimo mes: {ventas_emp1} y obtuvo una comisión de: ${comision_emp1}')
+# print(f'\n{empleado2} vendió el ultimo mes: {ventas_emp2} y obtuvo una comisión de: ${comision_emp2}\n')
+
+
+
+# Desafio 2
+
+nombre = input('Ingrese su nombre completo. Ej: Luis Suarez: ')
+edad = input('Ingrese su edad: ')
+estatura = input('Ingrese su estatura en metros. Ej: 1.8: ')
+peso = input('Ingrese su peso en kg. Ej: 65: ')
+direccion = input('Ingrese su dirección actual. Ej: La Madrid 756, Corrientes: ')
+tel = input('Ingrese su numero de telefono (sin 0 ni 15): ')
+
+print(f'\nNombre completo:[{nombre}]\nEdad:[{edad}]\nEstatura:[{estatura}]\nPeso:[{peso}Kg]\nDirección:[{direccion}]\nNúmero de teléfono:[{tel}]\n')
